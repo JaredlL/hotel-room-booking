cfc0fc783c068a4170c791997d6e01154b3ed745
jaredl <linden.jared@gmail.com>
2025-11-09 22:28:40 +0000

    Add DB migrations

---
+++ HotelRoomBooking/Data/Migrations/20251109214107_InitialCreate.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HotelRoomBooking.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RoomName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    RoomType = table.Column<string>(type: "text", nullable: false),
+                    Capacity = table.Column<int>(type: "integer", nullable: false),
+                    HotelName = table.Column<string>(type: "character varying(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Room_Hotels_HotelName",
+                        column: x => x.HotelName,
+                        principalTable: "Hotels",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    BookingReference = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    HotelName = table.Column<string>(type: "character varying(100)", nullable: false),
+                    BookedRoomId = table.Column<long>(type: "bigint", nullable: false),
+                    NumberOfGuests = table.Column<int>(type: "integer", nullable: false),
+                    GuestId = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.BookingReference);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Hotels_HotelName",
+                        column: x => x.HotelName,
+                        principalTable: "Hotels",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Room_BookedRoomId",
+                        column: x => x.BookedRoomId,
+                        principalTable: "Room",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookedNights",
+                columns: table => new
+                {
+                    RoomId = table.Column<long>(type: "bigint", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    BookingId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookedNights", x => new { x.RoomId, x.Date });
+                    table.ForeignKey(
+                        name: "FK_BookedNights_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "BookingReference",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookedNights_Room_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Room",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookedNights_BookingId",
+                table: "BookedNights",
+                column: "BookingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_BookedRoomId",
+                table: "Bookings",
+                column: "BookedRoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_HotelName",
+                table: "Bookings",
+                column: "HotelName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Room_HotelName",
+                table: "Room",
+                column: "HotelName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookedNights");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Room");
+
+            migrationBuilder.DropTable(
+                name: "Hotels");
+        }
+    }
+}
